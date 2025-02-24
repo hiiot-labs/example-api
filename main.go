295793d7e44524035002085423d46a86ed4c33f0
pirate <piraterain@outlook.com>
2025-02-24 16:56:51 +0800

    upgrade

--- main.go
+++ main.go
@@ -10,6 +10,9 @@ func main() {
 	http.HandleFunc("/test", func(writer http.ResponseWriter, request *http.Request) {
 		writer.Write([]byte("Hello world"))
 	})
+	http.HandleFunc("/test/v2", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte("Hello v2"))
+	})
 
 	fmt.Println("Listening on port 8899")
 	err := http.ListenAndServe(":8899", nil)
